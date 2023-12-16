6e13cefe7ad07c549b6155bf11f8de989cc129e3
Sandy M Moo Centeno <sandycenteno18@gmail.com>
2023-12-15 21:50:45 -0600

    initial files

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -41,14 +41,14 @@ const Card = (data) => {
 
   return (
     <div
-      className='bg-white cursor-pointer w-56 h-60 rounded-lg'
+      className='p-1 max-w-sm bg-white cursor-pointer w-56 h-60 rounded-lg'
       onClick={() => showProduct(data.data)}>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
         {renderIcon(data.data.id)}
       </figure>
-      <p className='flex justify-between'>
+      <p className='flex justify-between items-center'>
         <span className='text-sm font-light'>{data.data.title}</span>
         <span className='text-lg font-medium'>${data.data.price}</span>
       </p>
